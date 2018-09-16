7e615617e88ab051b85eb738a83c051178408af7
Elizabeth Karst <efkarst@outlook.com>
2018-09-16 15:58:22 -0700

    WashingtonHikes::Hike can return a list of all of the possible regions a user could hike in, scraped from WTA

--- lib/washington_hikes/hike.rb
+++ lib/washington_hikes/hike.rb
@@ -13,14 +13,14 @@ class WashingtonHikes::Hike
     scraped_hikes = WashingtonHikes::Scraper.new.scrape_wta_hike_list
     scraped_hikes.each do |hike|
       attributes = {
-        :name => hike.css(".item-header span").text.split(" - ")[0],
-        :region => hike.css(".item-header h3.region").text.split(" -- ")[0],
-        :length => hike.css(".hike-detail .hike-stats .hike-length span").children.text,
-        :elevation_gain => hike.css(".hike-detail .hike-stats .hike-gain span").children.text,
-        :rating => hike.css(".hike-detail .hike-stats .hike-rating .Rating .AverageRating .star-rating .current-rating").children.text,
-        :url => hike.css(".item-header a.listitem-title").attribute("href").value
+        :name => hike.css(".item-header span").text.split(" - ")[0].strip,
+        :region => hike.css(".item-header h3.region").text.split(" -- ")[0].strip,
+        :length => hike.css(".hike-detail .hike-stats .hike-length span").children.text.strip,
+        :elevation_gain => hike.css(".hike-detail .hike-stats .hike-gain span").children.text.strip,
+        :rating => hike.css(".hike-detail .hike-stats .hike-rating .Rating .AverageRating .star-rating .current-rating").children.text.strip,
+        :url => hike.css(".item-header a.listitem-title").attribute("href").value.strip
       }
-      self.new(attributes) if attributes != {}
+      self.new(attributes)
     end
   end
 
@@ -45,18 +45,12 @@ class WashingtonHikes::Hike
     puts ""
     puts "Enjoy fantastic views of the Blue Mountains from this cozy retreat on the edge of the Umatilla National Forest. \n Perched on an overlook above the Blue Mountains and just outside the Wenaha-Tucannon Wilderness in the Umatilla National Forest, the Wenatchee Guard Station was built by the Civilian Conservation Corps in 1934-1935. While many such historic structures have since been lost, this one has been maintained as a year-round forest service rental. Summer visitors will be able to drive right to the cabin, and winter visitors can access the building by ski, snowshoe, or snowmobile."
     puts "\n----------------------------\n \n"
-
-   # WashingtonHikes::CLI.what_next?("region")
   end
 
-    # Lists regions the user can choose from
-    def self.list_regions
-      #ideally this would call a Hike class method that returns a list of all regions
-      puts "1. North Cascades"
-      puts "2. Northeast Washington"
-      puts "3. Olympic Peninsula"
-      puts "4. Seattle and Puget Sound"
-      puts "5. Southeast Washington"
-      puts "6. Southwest Washington"
-    end
+  # Lists regions the user can choose from
+  def self.list_regions
+    regions = self.all.collect {|hike| hike.region}.uniq
+    regions.delete(nil)
+    regions.each.with_index(1) {|region,i| puts "#{i}. #{region}"}
+  end
 end
