973e9666d96f545b58c39878bc7e7da6553135cf
Elizabeth Karst <efkarst@outlook.com>
2018-09-23 09:37:42 -0700

    Refactor scrape_wta_hike_details to simplify code

--- lib/washington_hikes/scraper.rb
+++ lib/washington_hikes/scraper.rb
@@ -18,12 +18,12 @@ class WashingtonHikes::Scraper
       # Collect a hash of hike attributes for each hike on a page
       page_of_hikes.each do |hike|
         hike_attributes = {
-          :name => hike.css(".item-header span").text.split(" - ")[0].strip,
-          :region => WashingtonHikes::Region.find_or_create_region_by_name(hike.css(".item-header h3.region").text.split(" -- ")[0].strip),
-          :length => hike.css(".hike-detail .hike-stats .hike-length").size == 0 ? "unknown" : hike.css(".hike-detail .hike-stats .hike-length span").children.text.split(",")[0].split(" ")[0].to_i,
-          :type => hike.css(".hike-detail .hike-stats .hike-gain").size == 0 ? "unknown" : hike.css(".hike-detail .hike-stats .hike-gain span").children.text.strip.to_i,
-          :elevation_gain => hike.css(".hike-detail .hike-stats .hike-length").size == 0 ? "unknown" : hike.css(".hike-detail .hike-stats .hike-length span").children.text.split(",")[-1].strip,
-          :url => hike.css(".item-header a.listitem-title").attribute("href").value.strip
+          name:   hike.css(".item-header span").text.split(" - ")[0].strip,
+          region: WashingtonHikes::Region.find_or_create_region_by_name(hike.css(".item-header h3.region").text.split(" -- ")[0].strip),
+          url:    hike.css(".item-header a.listitem-title").attribute("href").value.strip,
+          length: hike.css(".hike-detail .hike-stats .hike-length").size == 0 ? "unknown" : hike.css(".hike-detail .hike-stats .hike-length span").children.text.split(",")[0].split(" ")[0].to_i,
+          type:   hike.css(".hike-detail .hike-stats .hike-gain").size == 0 ? "unknown" : hike.css(".hike-detail .hike-stats .hike-gain span").children.text.strip.to_i,
+          elevation_gain: hike.css(".hike-detail .hike-stats .hike-length").size == 0 ? "unknown" : hike.css(".hike-detail .hike-stats .hike-length span").children.text.split(",")[-1].strip
         }
         hikes << hike_attributes
       end      
@@ -33,26 +33,17 @@ class WashingtonHikes::Scraper
   end
 
   def self.scrape_wta_hike_details(url)
-    scraped_hike_details = get_page(url).css("#hike-wrapper")
+    details = get_page(url).css("#hike-wrapper")
 
-    hike_details = {
-      :features => scrape_wta_hike_features(scraped_hike_details),
-      :description => scraped_hike_details.css("#hike-body-text p").size == 0 ? "" : scraped_hike_details.css("#hike-body-text p")[0].text,
-      :rating => scraped_hike_details.css("#hike-top #hike-stats #hike-rating .Rating .AverageRating .star-rating .current-rating").text.split(" ")[0]
+    {
+      features: details.css("#hike-top #hike-features .feature").size == 0 ? [] : details.css("#hike-top #hike-features .feature").collect.with_index {|feature,i| feature.attribute("data-title").value},
+      description: details.css("#hike-body-text p").size == 0 ? "" : details.css("#hike-body-text p")[0].text,
+      rating: details.css("#hike-top #hike-stats #hike-rating .Rating .AverageRating .star-rating .current-rating").text.split(" ")[0]
     }
   end
 
-  def self.scrape_wta_hike_features(scraped_details)
-    # Extract hike feature list
-    feature_list = []
-    scraped_feature_list = scraped_details.css("#hike-top #hike-features .feature")
-    if scraped_feature_list != nil
-      scraped_feature_list.each.with_index do |feature,i|
-        puts scraped_feature_list.attribute("data-title").value
-        feature_list << scraped_feature_list[i].attribute("data-title").value
-      end
-    end
-    feature_list
-  end
+#  def self.scrape_wta_hike_features(scraped_details)
+#    scraped_details.css("#hike-top #hike-features .feature") != nil ? scraped_details.css("#hike-top #hike-features .feature").collect.with_index {|feature,i| scraped_feature_list[i].attribute("data-title").value} : []
+#  end
 
 end
